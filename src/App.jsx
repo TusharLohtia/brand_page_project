bd4f464d781ca93138b81342a243d9228f92ca9c
TusharLohtia <159414715+TusharLohtia@users.noreply.github.com>
2024-04-20 10:29:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css";
+import Navigation from './componnents/Navigation';
+import Hero from './componnents/Hero';
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App;
